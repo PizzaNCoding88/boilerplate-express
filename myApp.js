3c397559c8c18cb8f55fff0370af86106921deec
PizzaNCoding88 <umbertonardiello@gmail.com>
2024-03-18 17:21:42 +0000

    handler

--- myApp.js
+++ myApp.js
@@ -1,8 +1,11 @@
 let express = require('express');
+const res = require('express/lib/response');
 const { userPassedConsoleChallenge } = require('fcc-express-bground/globals');
 let app = express();
 
-
+app.get("/", (req,res) =>{
+    res.send("Hello Express");
+})
 
 
 
