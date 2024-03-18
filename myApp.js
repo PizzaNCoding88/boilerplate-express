77f0ef8c0d5a1ba08571f0f6beea5f74b6bae011
PizzaNCoding88 <umbertonardiello@gmail.com>
2024-03-18 17:52:46 +0000

    json

--- myApp.js
+++ myApp.js
@@ -15,8 +15,9 @@ let path = __dirname + '/public';
 // app.use("/public", express.static(__dirname + "/public");
 app.use('/public', express.static(path));
 
-
-
+app.get("/json", (req,res) => {
+    res.json({"message" : "Hello json"});
+})
 
 
 
